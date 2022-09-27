c711665a47caaa75669af7cbf4bdfcf9cf2b1477
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2022-09-27 19:13:39 -0400

    Worked with components

---
+++ frontend/src/button.component.jsx
@@ -0,0 +1,11 @@
+import "./button.styles.css";
+
+const Button = ({ text }) => {
+    return (
+        <button className="button">
+            {text}
+        </button>
+    )
+}
+
+export default Button;
