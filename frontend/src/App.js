bd84ffef87e9aa8502eb61010230db7154fcf7da
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-10-24 19:54:54 -0400

    chore: converted 2-space indents to 4-space

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,48 +4,48 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.user = {  // Not a real user from db, just for display testing
-      "id": 912345678,
-      "firstName": "George P.",
-      "lastName": "Burdell",
-      "email": "gburdell@gatech.edu",
-      "major": ["Computer Science"],
-      "minor": [],
-      "classification": "Senior",
-      "earnedCredits": 126,
-      "gpa": 4.0,
-      "coursesTaken": [
-        {
-          "id": "GT 1000",
-          "semester": "Fall 2022",
-          "grade": "A"
+    constructor(props) {
+        super(props)
+        this.user = {  // Not a real user from db, just for display testing
+            "id": 912345678,
+            "firstName": "George P.",
+            "lastName": "Burdell",
+            "email": "gburdell@gatech.edu",
+            "major": ["Computer Science"],
+            "minor": [],
+            "classification": "Senior",
+            "earnedCredits": 126,
+            "gpa": 4.0,
+            "coursesTaken": [
+                {
+                    "id": "GT 1000",
+                    "semester": "Fall 2022",
+                    "grade": "A"
+                }
+            ]
         }
-      ]
     }
-  }
 
-  render() {
-    return (
-      <Router>
-        <TopBar user={this.user}/>
-        <Routes>
-          <Route path="/user"> {/* Do NOT go to empty paths right now, as they will induce an infinite loop in browser*/}
-            
-          </Route>
-          <Route path="/course/:courseId">
-            
-          </Route>
-          <Route path="/junior-design-options">
-            
-          </Route>
-          <Route path="/" element={<HomePage/>}>
-          </Route>
-        </Routes>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <TopBar user={this.user}/>
+                <Routes>
+                    <Route path="/user"> {/* Do NOT go to empty paths right now, as they will induce an infinite loop in browser*/}
+                        
+                    </Route>
+                    <Route path="/course/:courseId">
+                        
+                    </Route>
+                    <Route path="/junior-design-options">
+                        
+                    </Route>
+                    <Route path="/" element={<HomePage/>}>
+                    </Route>
+                </Routes>
+            </Router>
+        );
+    }
 }
 
 export default App;
