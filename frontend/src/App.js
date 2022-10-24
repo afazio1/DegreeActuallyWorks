88c3be87a71649282f116ae672cdb66a92656ba0
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-10-24 19:51:32 -0400

    minor: Change App to class component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,51 @@
 import TopBar from './components/topBar.component'
 import HomePage from './components/homePage.component';
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
-  const testUser = {  // Not a real user from db, just for display testing
-    "id": 912345678,
-    "firstName": "George P.",
-    "lastName": "Burdell",
-    "email": "gburdell@gatech.edu",
-    "major": ["Computer Science"],
-    "minor": [],
-    "classification": "Senior",
-    "earnedCredits": 126,
-    "gpa": 4.0,
-    "coursesTaken": [
-      {
-        "id": "GT 1000",
-        "semester": "Fall 2022",
-        "grade": "A"
-      }
-    ]
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.user = {  // Not a real user from db, just for display testing
+      "id": 912345678,
+      "firstName": "George P.",
+      "lastName": "Burdell",
+      "email": "gburdell@gatech.edu",
+      "major": ["Computer Science"],
+      "minor": [],
+      "classification": "Senior",
+      "earnedCredits": 126,
+      "gpa": 4.0,
+      "coursesTaken": [
+        {
+          "id": "GT 1000",
+          "semester": "Fall 2022",
+          "grade": "A"
+        }
+      ]
+    }
   }
 
-  return (
-    <Router>
-      <TopBar user={testUser}/>
-      <Routes>
-        {/* <Route path="/user">
-          
-        </Route>
-        <Route path="/course/:courseId">
-          
-        </Route>
-        <Route path="/junior-design-options">
-          
-        </Route> */}
-        <Route path="/" element={<HomePage/>}>
-        </Route>
-      </Routes>
-    </Router>
-  );
+  render() {
+    return (
+      <Router>
+        <TopBar user={this.user}/>
+        <Routes>
+          <Route path="/user"> {/* Do NOT go to empty paths right now, as they will induce an infinite loop in browser*/}
+            
+          </Route>
+          <Route path="/course/:courseId">
+            
+          </Route>
+          <Route path="/junior-design-options">
+            
+          </Route>
+          <Route path="/" element={<HomePage/>}>
+          </Route>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
